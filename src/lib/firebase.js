b5afcf3ca009b61f3fb872138ffb22a5f5a60515
Jopex Creatives <jopexsly@gmail.com>
2026-08-21 19:21:23 +0000

    Last Update

--- src/lib/firebase.js
+++ src/lib/firebase.js
@@ -4,13 +4,13 @@ import { getFirestore } from "firebase/firestore";
 import { getStorage } from "firebase/storage";
 
 const firebaseConfig = {
-  apiKey: "AIzaSyBwyUZM2nLM5M3Tmw4YC8DwtvBziiWzLXA",
-  authDomain: "infy-app-8119a.firebaseapp.com",
-  projectId: "infy-app-8119a",
-  storageBucket: "infy-app-8119a.firebasestorage.app",
-  messagingSenderId: "440440351373",
-  appId: "1:440440351373:web:c9628c4360dbf001aa90c4",
-  measurementId: "G-3FR0SNB3DE"
+  apiKey: process.env.NEXT_PUBLIC_FIREBASE_API_KEY || "",
+  authDomain: process.env.NEXT_PUBLIC_FIREBASE_AUTH_DOMAIN || "",
+  projectId: process.env.NEXT_PUBLIC_FIREBASE_PROJECT_ID || "",
+  storageBucket: process.env.NEXT_PUBLIC_FIREBASE_STORAGE_BUCKET || "",
+  messagingSenderId: process.env.NEXT_PUBLIC_FIREBASE_MESSAGING_SENDER_ID || "",
+  appId: process.env.NEXT_PUBLIC_FIREBASE_APP_ID || "",
+  measurementId: process.env.NEXT_PUBLIC_FIREBASE_MEASUREMENT_ID || ""
 };
 
 // Initialize Firebase
